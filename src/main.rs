7c4cb31eb7e672980e73fa0ef1df71c1f7a64e90
Leonardo C <leonardorudolfocardoso@gmail.com>
2022-08-28 18:56:22 -0300

    first_word function

--- src/main.rs
+++ src/main.rs
@@ -8,4 +8,14 @@ fn main() {
     let mut buf = String::new();
     stdin().read_line(&mut buf)
         .expect("Not able to read input. ");
+
+
+    println!("First word is \"{}\"", first_word(&buf));
+}
+
+fn first_word (s: &String) -> &str {
+    match s.split_whitespace().next() {
+        Some(word) => word,
+        None => s
+    }
 }
