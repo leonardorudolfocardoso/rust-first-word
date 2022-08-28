f2a29131626863087accd3b4ec49bafe560e42fd
Leonardo C <leonardorudolfocardoso@gmail.com>
2022-08-28 18:40:19 -0300

    read user input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::io::stdin;
+
 fn main() {
-    println!("Hello, world!");
+    println!("First word");
+
+    println!("Enter your input:");
+
+    let mut buf = String::new();
+    stdin().read_line(&mut buf)
+        .expect("Not able to read input. ");
 }
